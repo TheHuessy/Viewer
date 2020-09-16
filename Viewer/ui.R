e225370ef661cb91c26f842d2ad899e91f3d5964
James Huessy <jameshuessy@gmail.com>
2020-09-15 22:55:26 -0400

    cleanup

---
+++ Viewer/ui.R
@@ -0,0 +1,40 @@
+## THIS: https://livefreeordichotomize.com/2017/03/12/introducing-shinyswipr-swipe-your-way-to-a-great-shiny-ui/
+
+## HOLY SHIT, ALSO THIS: https://github.com/RinteRface/cheatsheets/blob/master/shinyMobile/shinyMobile.pdf
+
+library(shiny)
+library(shinyWidgets)
+library(magick)
+
+
+shinyUI(
+        fixedPage(
+                  ## GET RID OF SIDEBAR
+                  tags$head(tags$script('
+                                        var dimension = [0, 0];
+                                        $(document).on("shiny:connected", function(e) {
+                                                         dimension[0] = window.innerWidth;
+                                                         dimension[1] = window.innerHeight;
+                                                         Shiny.onInputChange("dimension", dimension);});
+                                        $(window).resize(function(e) {
+                                                           dimension[0] = window.innerWidth;
+                                                           dimension[1] = window.innerHeight;
+                                                           Shiny.onInputChange("dimension", dimension);});')
+                                        ),
+
+                                        includeCSS("style.css"),
+                                        div(style="display: inline-block;vertical-align:top; width: 125px;",
+                                            ## In order for these to be side by side, you need this div with this sort of style setting
+                                            switchInput(inputId = "auto_toggle_btn",
+                                                        onLabel = "Auto",
+                                                        value = FALSE)
+                                            ),
+                                        div(style="display: inline-block;vertical-align:top; width: 125px;",
+                                            actionButton("kill_switch", "X")
+                                            ),
+                                        imageOutput("image_output",
+                                                    click = "im_click")
+)
+                  )
+
+
---
+++ viewer_runner.R
@@ -0,0 +1,16 @@
+library(shiny)
+
+options(shiny.host = "192.168.0.113")
+options(shiny.port = 6514) 
+
+# Adjust the path to match where the server file is.
+# It looks like it starts the kernel in Documents on this machine when running Rstudio
+
+#runApp('Jupyter Notebooks/Mobile Culler/Swiper')
+
+# Running the runner as a standalone script from the command prompt
+# Standalone requires relative path
+  # Also requires user access control toggling in Windows, which is not ideal
+    # Probs won't be an issue on linux
+
+runApp('Viewer')
